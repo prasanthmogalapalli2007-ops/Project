d9902b893b692a653788ff32be96b6e43f706360
prasanthmogalapalli2007-ops <prasanthmogalapalli2007@gmail.com>
2026-03-16 00:08:51 +0530

    uc11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,38 +7,55 @@ public class PalindromeCheckerApp {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=================================");
-        System.out.println("   Palindrome Checker - UC4");
+        System.out.println(" Palindrome Checker - UC11");
+        System.out.println(" Object-Oriented Service Version");
         System.out.println("=================================");
 
-        // Taking input from user
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        // Convert String to Character Array
-        char[] characters = input.toCharArray();
+        // Create object of service class
+        PalindromeChecker checker = new PalindromeChecker();
 
-        // Two-pointer approach
-        int start = 0;
-        int end = characters.length - 1;
+        boolean result = checker.checkPalindrome(input);
 
-        boolean isPalindrome = true;
-
-        while (start < end) {
-            if (characters[start] != characters[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-        // Display result
-        if (isPalindrome) {
+        if (result) {
             System.out.println("Result: The given string is a Palindrome.");
         } else {
             System.out.println("Result: The given string is NOT a Palindrome.");
         }
 
         scanner.close();
+    }
+}
+
+// Service class following OOP principles
+class PalindromeChecker {
+
+    // Public method exposed to user
+    public boolean checkPalindrome(String input) {
+
+        if (input == null) {
+            return false;
+        }
+
+        return isPalindrome(input);
+    }
+
+    // Private internal logic (Encapsulation)
+    private boolean isPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
